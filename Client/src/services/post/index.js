65e1e667f35375810e67117c899fbec82b53056c
ByeonHajin <gkwls1130@ajou.ac.kr>
2022-04-15 20:24:59 +0900

    TEST: Post 서버연동 Dummy data 테스트

--- Client/src/services/post/index.js
+++ Client/src/services/post/index.js
@@ -7,6 +7,10 @@ export const postData = async(postData) => {
     const title = postData.title;
     const restaurant = postData.restaurant;
 
+    const headers = {
+        'Access-Control-Allow-Origin' : "*"
+    }
+
     try {
         const response = await axios.post(POST_BASE_URL, {
             title: '1',
@@ -16,7 +20,7 @@ export const postData = async(postData) => {
             p_location: '2',
             u_id: 1,
             r_id: 1,
-        })
+        }, {headers:headers}, {withCredentials: false});
         console.log(response);
     } catch(error) {
         console.error(error);
